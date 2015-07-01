1d5373703afadda71a339a388d89c57a09f39cba
Mikael Lindqvist <li.mikael@gmail.com>
2015-07-01 19:14:16 +0200

    don't set cookie stored tab if target is specified in url

--- tabby-cookie.js
+++ tabby-cookie.js
@@ -1,5 +1,24 @@
 jQuery(function($) {
 	$(document).ready(function() {
+		function getQueryStringParams() {
+			var params = {};
+			(function() {
+
+				var match,
+					pl = /\+/g, // Regex for replacing addition symbol with a space
+					search = /([^&=]+)=?([^&]*)/g,
+					decode = function(s) {
+						return decodeURIComponent(s.replace(pl, " "));
+					},
+					query = window.location.search.substring(1).replace(/amp;/g, "");
+
+				while (match = search.exec(query))
+					params[decode(match[1])] = decode(match[2]);
+			})();
+
+			return params;
+		}
+
 		function setCookie(key, value) {
 			var expires = new Date();
 			expires.setTime(expires.getTime() + (1 * 24 * 60 * 60 * 1000));
@@ -15,9 +34,11 @@ jQuery(function($) {
 			setCookie("tabby-cookie",$(this).text());
 		});
 
-		$(".responsive-tabs__list__item").each(function(index, li) {
-			if (getCookie("tabby-cookie")==$(li).text())
-				li.click();
-		});
+		if (!getQueryStringParams().target) {
+			$(".responsive-tabs__list__item").each(function(index, li) {
+				if (getCookie("tabby-cookie")==$(li).text())
+					li.click();
+			});
+		}
 	});
 });
